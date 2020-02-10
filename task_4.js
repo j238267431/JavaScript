7bd1be910a6b674b66c4971e2d24e50a2d0e1b10
Evgenij Jakovlev <j238267431@mail.ru>
2020-02-10 16:14:34 +0300

    сделал задание 4

--- task_4.js
+++ task_4.js
@@ -1,43 +1,34 @@
 'use strict';
-/**
- * Функция складывает 2 числа
- * @param {Number} a первое слагаемое
- * @param {Number} b второе слагаемое
- */
-function getSum(a,b){
-   return a+b;
-}
-/**
- * Функция умножает 2 числа
- * @param {Number} a первый множитель
- * @param {Number} b второй множитель
- */
-function getMultiplying(a,b){
-   return a*b;
-}
-/**
- * Функция делит одно число на другое
- * @param {Number} a делимое
- * @param {Number} b делитель
- */
-function getDividing(a,b){
-   return a/b;
-}
-/**
- * Функция вычитания
- * @param {Number} a от этого числа отнимаем 
- * @param {Number} b это число отнимаем
- */
-function getSubstraction(a,b){
-   return a-b;
-}
 
-console.log(getSum(2,3));
-console.log(getMultiplying(3,4));
-console.log(getDividing(5,3));
-console.log(getSubstraction(10,7));
-// 4 Реализовать основные 4 арифметические операции (+, -, /, *) в виде функций с двумя
-// параметрами. Т.е. например, функция для сложения должна принимать два числа, складывать их
-// и возвращать результат.
-// Обязательно использовать оператор return.
 
+products = [
+    {
+        id: 3,
+        price: 127,
+        photos: ["1.jpg", "2.jpg"]
+    }, {
+        id: 5,
+        price: 499,
+        photos: []
+    }, {
+        id: 10,
+        price: 26,
+        photos: ["3.jpg"]
+    }, {
+        id: 8,
+        price: 78
+    }
+];
+
+
+console.log(products.filter(elem => elem.photos && elem.photos.length > 0));
+console.log(products.sort((elem1,elem2) => elem1.price-elem2.price))
+
+
+
+// 5 Перед вами находится массив с продуктами в интернет - магазине.Вам нужно : 1 Получить все товары,
+// у которых есть фотографии,
+// можете использовать метод filter https : //mzl.la/2qROQkT
+// 2 Отсортируйте товары по цене(от низкой цены к высокой),
+// можете использовать метод sort
+// https : //mzl.la/2Y79hbZ
