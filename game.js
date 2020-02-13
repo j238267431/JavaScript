24356d44293073dff7c1314d90e0fdcbba3b81be
Evgenij Jakovlev <j238267431@mail.ru>
2020-02-13 16:38:13 +0300

    js файлы для игры бродилка

---
+++ game.js
@@ -0,0 +1,25 @@
+let game = {
+    run() {
+        while (true) {
+            const direction = mover.getDirection();
+            if (direction == null){
+               console.log("Игра окончена");
+               return;
+            }
+            const nextPoint = mover.getNextPosition(direction)
+            renderer.clear();
+            
+            player.move(nextPoint);
+            renderer.render();
+        }
+
+    },
+
+    init() {
+        console.log("ваше положение наполе в виде о");
+        renderer.render();
+        console.log("чтобы начать игру наберите game.run() и нажмите Enter")
+    }
+};
+
+game.init();
